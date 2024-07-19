e9624801022de1b63c0bfe7cf187670387c88ce6
laffed <roarklaffed@gmail.com>
2024-07-19 13:49:15 -0500

    v2

--- src/main.rs
+++ src/main.rs
@@ -1,137 +1,272 @@
 use std::cmp::Ordering;
-use std::io;
+use std::io::stdin;
 
-struct ScoreState {
-    serving_team: TeamFlag,
-    serving_rank: ServingRank,
-    score_red: u8,
-    score_blue: u8,
-    grid_red: (Player, Player),
-    grid_blue: (Player, Player),
+fn main() {
+    let mut score = Score::new();
+
+    loop {
+        let mut input = String::new();
+        score.announce_score();
+
+        println!("input point winner:");
+        stdin().read_line(&mut input).expect("Failed to read input");
+        let point_winner = is_valid_input(&input);
+
+        match point_winner {
+            Some(pw) => {
+                score.calc_point(pw);
+            }
+            None => {
+                println!("Invalid input. Please try again.");
+                continue;
+            }
+        }
+
+        match score.is_game_over() {
+            Some(winner) => {
+                println!("Game over! Winner is: {:?}", winner);
+                break;
+            }
+            None => continue,
+        }
+    }
 }
 
-enum TeamFlag {
+const SCORE_SOFT_CAP: u32 = 11;
+const DUECE_WIN_DIFF: u32 = 2;
+
+const VALID_SERV_INPUT: [&str; 3] = ["0", "serv", "serving"];
+const VALID_REC_INPUT: [&str; 3] = ["1", "rec", "receiving"];
+
+fn is_valid_input(input: &str) -> Option<RelativeSide> {
+    let input = input.trim();
+    for s in VALID_SERV_INPUT.iter() {
+        if input == *s {
+            return Some(RelativeSide::Serving);
+        }
+    }
+    for r in VALID_REC_INPUT.iter() {
+        if input == *r {
+            return Some(RelativeSide::Receiving);
+        }
+    }
+
+    None
+}
+
+struct Score {
+    serving: Server,
+    red: u32,
+    blue: u32,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+enum Server {
+    Red1,
+    Red2,
+    Blue1,
+    Blue2,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+enum Team {
     Red,
     Blue,
 }
 
-#[derive(Debug, Copy, Clone)]
-
-enum Player {
-    A,
-    B,
+#[derive(Debug, PartialEq, Eq, Clone)]
+enum RelativeSide {
+    Serving,
+    Receiving,
 }
 
-enum ServingRank {
-    First,
-    Second,
-}
-
-fn flip_grid(grid: (Player, Player)) -> (Player, Player) {
-    (grid.1, grid.0)
-}
-
-fn point(state: &mut ScoreState) {
-    match state.serving_team {
-        TeamFlag::Red => {
-            state.score_red += 1;
-            state.grid_red = flip_grid(state.grid_red);
-        }
-        TeamFlag::Blue => {
-            state.score_blue += 1;
-            state.grid_blue = flip_grid(state.grid_blue);
+impl Score {
+    pub fn new() -> Self {
+        Score {
+            serving: Server::Red2,
+            red: 0,
+            blue: 0,
         }
     }
-    println!("Point.")
-}
 
-fn break_point(state: &mut ScoreState) {
-    match state.serving_rank {
-        ServingRank::First => {
-            state.serving_rank = ServingRank::Second;
-            println!("[Block Point]: 2nd Serve");
+    pub fn announce_score(&self) {
+        match self.serving {
+            Server::Red1 => println!("Score: Red {}, {}, {}", self.red, self.blue, 1),
+            Server::Red2 => println!("Score: Red {}, {}, {}", self.red, self.blue, 2),
+            Server::Blue1 => println!("Score: Blue {}, {}, {}", self.blue, self.red, 1),
+            Server::Blue2 => println!("Score: Blue {}, {}, {}", self.blue, self.red, 2),
+        };
+    }
+
+    fn calc_next_server(&self, point_winner: RelativeSide) -> Server {
+        match &self.serving {
+            Server::Red1 => match point_winner {
+                RelativeSide::Serving => Server::Red1,
+                RelativeSide::Receiving => Server::Red2,
+            },
+            Server::Red2 => match point_winner {
+                RelativeSide::Serving => Server::Red2,
+                RelativeSide::Receiving => Server::Blue1,
+            },
+            Server::Blue1 => match point_winner {
+                RelativeSide::Serving => Server::Blue1,
+                RelativeSide::Receiving => Server::Blue2,
+            },
+            Server::Blue2 => match point_winner {
+                RelativeSide::Serving => Server::Blue2,
+                RelativeSide::Receiving => Server::Red1,
+            },
         }
-        ServingRank::Second => {
-            state.serving_rank = ServingRank::First;
-            match state.serving_team {
-                TeamFlag::Red => {
-                    state.serving_team = TeamFlag::Blue;
-                }
-                TeamFlag::Blue => {
-                    state.serving_team = TeamFlag::Red;
-                }
-            }
-            println!("[Break Point]: Side Out");
+    }
+
+    pub fn is_game_over(&self) -> Option<Team> {
+        if self.red < SCORE_SOFT_CAP && self.blue < SCORE_SOFT_CAP {
+            return None;
+        };
+
+        let leading_team = match self.red.cmp(&self.blue) {
+            Ordering::Greater => Team::Red,
+            Ordering::Less => Team::Blue,
+            Ordering::Equal => return None,
+        };
+
+        match self.red.abs_diff(self.blue) >= DUECE_WIN_DIFF {
+            false => None,
+            true => Some(leading_team),
         }
+    }
+
+    pub fn calc_point(&mut self, point_winner: RelativeSide) {
+        let increment: u32 = if point_winner == RelativeSide::Serving {
+            1
+        } else {
+            0
+        };
+
+        match self.serving {
+            Server::Red1 | Server::Red2 => self.red += increment,
+            Server::Blue1 | Server::Blue2 => self.blue += increment,
+        };
+
+        self.serving = self.calc_next_server(point_winner);
     }
 }
 
-fn main() {
-    let mut state = ScoreState {
-        serving_team: TeamFlag::Red,
-        serving_rank: ServingRank::Second,
-        score_red: 0,
-        score_blue: 0,
-        grid_blue: (Player::B, Player::A),
-        grid_red: (Player::B, Player::A),
-    };
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    println!("Game Start!");
-    println!("Score: 0, 0, 2");
-    println!("       Blu{:?}", state.grid_blue);
-    println!("       Red{:?} <- Serving Side", state.grid_red);
+    #[test]
+    fn test_is_valid_input() {
+        assert_eq!(is_valid_input("0"), Some(RelativeSide::Serving));
+        assert_eq!(is_valid_input("serv"), Some(RelativeSide::Serving));
+        assert_eq!(is_valid_input("serving"), Some(RelativeSide::Serving));
+        assert_eq!(is_valid_input("1"), Some(RelativeSide::Receiving));
+        assert_eq!(is_valid_input("rec"), Some(RelativeSide::Receiving));
+        assert_eq!(is_valid_input("receiving"), Some(RelativeSide::Receiving));
+        assert_eq!(is_valid_input("invalid"), None);
+    }
 
-    loop {
-        let mut input = String::new();
-        println!("");
-
-        io::stdin()
-            .read_line(&mut input)
-            .expect("Failed to read line");
-
-        let input: u8 = input.trim().parse().expect("Input must be a number");
-
-        let input: TeamFlag = match input.cmp(&0) {
-            Ordering::Less | Ordering::Equal => TeamFlag::Blue,
-            Ordering::Greater => TeamFlag::Red,
+    #[test]
+    fn test_calc_next_server() {
+        let score = Score {
+            serving: Server::Red1,
+            red: 0,
+            blue: 0,
         };
+        assert_eq!(score.calc_next_server(RelativeSide::Serving), Server::Red1);
+        assert_eq!(
+            score.calc_next_server(RelativeSide::Receiving),
+            Server::Red2
+        );
 
-        match state.serving_team {
-            TeamFlag::Blue => match input {
-                TeamFlag::Blue => point(&mut state),
-                TeamFlag::Red => break_point(&mut state),
-            },
-            TeamFlag::Red => match input {
-                TeamFlag::Red => point(&mut state),
-                TeamFlag::Blue => break_point(&mut state),
-            },
+        let score = Score {
+            serving: Server::Red2,
+            red: 0,
+            blue: 0,
         };
+        assert_eq!(score.calc_next_server(RelativeSide::Serving), Server::Red2);
+        assert_eq!(
+            score.calc_next_server(RelativeSide::Receiving),
+            Server::Blue1
+        );
 
-        let f_score = match state.serving_team {
-            TeamFlag::Red => &state.score_red,
-            TeamFlag::Blue => &state.score_blue,
+        let score = Score {
+            serving: Server::Blue1,
+            red: 0,
+            blue: 0,
         };
+        assert_eq!(score.calc_next_server(RelativeSide::Serving), Server::Blue1);
+        assert_eq!(
+            score.calc_next_server(RelativeSide::Receiving),
+            Server::Blue2
+        );
 
-        let s_score = match state.serving_team {
-            TeamFlag::Red => &state.score_blue,
-            TeamFlag::Blue => &state.score_red,
+        let score = Score {
+            serving: Server::Blue2,
+            red: 0,
+            blue: 0,
         };
+        assert_eq!(score.calc_next_server(RelativeSide::Serving), Server::Blue2);
+        assert_eq!(
+            score.calc_next_server(RelativeSide::Receiving),
+            Server::Red1
+        );
+    }
 
-        let r_score = match state.serving_rank {
-            ServingRank::First => 1,
-            ServingRank::Second => 2,
+    #[test]
+    fn is_game_over_true() {
+        let score = Score {
+            serving: Server::Red1,
+            red: 11,
+            blue: 9,
         };
+        assert_eq!(score.is_game_over(), Some(Team::Red));
+        let score = Score {
+            serving: Server::Blue1,
+            red: 9,
+            blue: 11,
+        };
+        assert_eq!(score.is_game_over(), Some(Team::Blue));
+        let score = Score {
+            serving: Server::Red2,
+            red: 14,
+            blue: 12,
+        };
+        assert_eq!(score.is_game_over(), Some(Team::Red));
+        let score = Score {
+            serving: Server::Blue2,
+            red: 20,
+            blue: 22,
+        };
+        assert_eq!(score.is_game_over(), Some(Team::Blue));
+    }
 
-        println!("Score: {}, {}, {}", f_score, s_score, r_score);
-        match state.serving_team {
-            TeamFlag::Red => {
-                println!("       Blu{:?}", state.grid_blue);
-                println!("       Red{:?}", state.grid_red);
-            }
-            TeamFlag::Blue => {
-                println!("       Red{:?}", state.grid_red);
-                println!("       Blu{:?}", state.grid_blue);
-            }
-        }
+    #[test]
+    fn is_game_over_false() {
+        let score = Score {
+            serving: Server::Red1,
+            red: 10,
+            blue: 9,
+        };
+        assert_eq!(score.is_game_over(), None);
+        let score = Score {
+            serving: Server::Blue1,
+            red: 9,
+            blue: 10,
+        };
+        assert_eq!(score.is_game_over(), None);
+        let score = Score {
+            serving: Server::Red2,
+            red: 13,
+            blue: 12,
+        };
+        assert_eq!(score.is_game_over(), None);
+        let score = Score {
+            serving: Server::Blue2,
+            red: 21,
+            blue: 22,
+        };
+        assert_eq!(score.is_game_over(), None);
     }
 }
